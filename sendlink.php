ed6878bd82da7912e1a253b89468f5b31bb6ca92
brwinzap <50934216+brwinzap@users.noreply.github.com>
2019-05-22 22:48:05 -0300

    Update sendlink.php

--- sendlink.php
+++ sendlink.php
@@ -12,13 +12,13 @@ $to = "5511900000000@c.us"; // Its phone number with country prefix. tag: @c.us 
 $thumb = "https://i.imgur.com/7Cm1HSp.png";
 $title = "Title";
 $desc = "Description";
-$link = "https://winzap.com.br"; // Image URL.Use with: cmd = "image"	
+$link = "https://winzap.com.br";
 $ch = curl_init();	
 curl_setopt($ch, CURLOPT_URL, "https://winzap.me/api");	
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);	
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);	
 curl_setopt($ch, CURLOPT_POST, 1);	
-curl_setopt($ch, CURLOPT_POSTFIELDS, "token=$token&cmd=$cmd&id=$id&to=$to&msg=$msg&link=$link");	
+curl_setopt($ch, CURLOPT_POSTFIELDS, "token=$token&cmd=$cmd&id=$id&to=$to&thumb=$thumb&title=$title&desc=$desc&link=$link");	
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);	
 curl_setopt($ch, CURLOPT_MAXREDIRS, 5);	
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);	
